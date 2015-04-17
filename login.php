158a26bc50ce4740b9e5dea9ddd01c1b51a20837
Thomas Kesman <kes15779@spengergasse.at>
2015-04-17 11:50:07 +0200

    login fix after merge

--- login.php
+++ login.php
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-<?php
-include_once 'include_login.php';
-include_once 'DB_Con.php';
-if(!isset($_SESSION))
-	session_start();
-//test:
-//$_SESSION[L_ANGEMELDET]=true;
-//echt:
-$success = true;
-if(!isset($_SESSION[L_USERNAME])){
-	if(!login($_POST[L_USERNAME], $_POST[L_PASSWORT]))
-		$success = false;
-}
-if ($success) {
-	$db = new DB_Con(DB_DEFAULT_CONF_FILE, true);
-	$success = $db->sessionEintragen(session_id(), $_POST[L_USERNAME]);
-}
-echo json_encode($success?array("sessioId"=>session_id()):array("success"=>$success));
-=======
 <?php
 include_once 'include_login.php';
 include_once 'DB_Con.php';
@@ -45,6 +25,5 @@ if ($success) {
 	$db = new DB_Con(DB_DEFAULT_CONF_FILE, true);
 	$success = $db->sessionEintragen(session_id(), $_POST[L_USERNAME]);
 }
-echo json_encode($success?session_id():$success);
->>>>>>> 5f6764d91509fdd92174bf26627fded60f20412d
+echo json_encode($success?array("sessioId"=>session_id()):array("success"=>$success));
 session_write_close();
