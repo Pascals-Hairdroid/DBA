a7cca8c17dff7be56765f72d8c9ed7f896f0bb6d
oli9k9 <oli9k9@hotmail.com>
2015-05-22 12:14:40 +0200

    classes/DB_Exception.php geändert: get- und set-Methoden implementiert (mit UTF8 en- und decoding);; ;; ;;;

--- classes/DB_Exception.php
+++ classes/DB_Exception.php
@@ -5,10 +5,53 @@ class DB_Exception extends Exception{
 	public $viewmsg; 	// User View Message
 	
 	function __construct($errc, $msg, $viewmsg){
-		$this->message = $msg;
-		$this->errc = $errc;
-		$this->msg = $msg;
-		$this->viewmsg = $viewmsg;
+		$this->setErrc($errc);
+		$this->setMsg($msg);
+		$this->setViewmsg($viewmsg);
 	}
+	
+	function setErrc($errc){
+		try{
+			$errc=(int)$errc;
+		}
+		catch(Exception $e){}
+		if(is_int($errc))
+			$this->errc = $errc;
+		else
+			throw new Exception("Errorcode ungültig!");
+	}
+	
+	function setMsg($msg){
+		try{
+			$msg = (string)$msg;
+		}catch (Exception $e){}
+		if(!is_string($msg))
+			throw new Exception("Message ungültig!");
+		$this->msg = utf8_encode($msg);
+		$this->message = $this->msg;
+	}
+	
+	function setViewmsg($viewmsg){
+		try{
+			$viewmsg = (string)$viewmsg;
+		}catch (Exception $e){}
+		if(!is_string($viewmsg))
+			throw new Exception("Viewmessage ungültig!");
+		$this->viewmsg = utf8_encode($viewmsg);
+	}
+	
+	
+	function getErrc(){
+		return $this->errc;
+	}
+	
+	function getMsg(){
+		return utf8_decode($this->msg);
+	}
+	
+	function getViewmsg(){
+		return utf8_decode($this->viewmsg);
+	}
+	
 }
 ?>
