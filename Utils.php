000d0f4874d09ffe7d81f5eaf8c3285b227bf037
oli9k9 <oli9k9@hotmail.com>
2015-06-19 11:10:33 +0200

    classes/Werbung.php geändert: Funktion exists($url) wird jetzt outgesourced;; ;; ;;; Utils.php geändert: Funktion exists($url) eingefügt;; ;; ;;;

--- Utils.php
+++ Utils.php
@@ -89,6 +89,17 @@ function resizeImage($image, $width, $height){
 	return $img;
 }
 
+function exists($url){
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL,$url);
+	// don't download content
+	curl_setopt($ch, CURLOPT_NOBODY, 1);
+	curl_setopt($ch, CURLOPT_FAILONERROR, 1);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	if(curl_exec($ch)!==FALSE)
+		return true;
+	return false;
+}
 
 function umlaute_encode($str){
 	$a = array("ä", "ö", "ü", "Ä", "Ö", "Ü");
--- classes/Werbung.php
+++ classes/Werbung.php
@@ -1,23 +1,8 @@
 <?php
 include_once dirname(__FILE__)."/".'../conf/db_const.php';
 include_once dirname(__FILE__)."/".'Interesse.php';
+include_once dirname(__FILE__)."/../".'Utils.php';
 
-function exists($url){
-  $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL,$url);
-    // don't download content
-    curl_setopt($ch, CURLOPT_NOBODY, 1);
-    curl_setopt($ch, CURLOPT_FAILONERROR, 1);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    if(curl_exec($ch)!==FALSE)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
 
 class Werbung {
 	public $nummer;
